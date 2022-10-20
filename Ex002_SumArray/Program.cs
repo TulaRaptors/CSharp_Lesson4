4762e4b8cbc1df2ff57193c1113bf3f28a835ac9
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 16:07:56 +0300

    Сумма полож./отр. элементов массива

---
+++ Ex002_SumArray/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0,10);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write($"{element} ");
+}
+
+int SumPositiveArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int SumNegativeArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+
+int[] array = new int[12];
+FillArray(array);
+Console.Write("Массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов = {SumPositiveArray(array)}");
+Console.WriteLine($"Сумма отрицательных элементов = {SumNegativeArray(array)}");
