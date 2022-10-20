049f6098538f942ecedd244ac2920e987c94064f
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 16:21:04 +0300

    Количество четных/нечетных чисел в массиве

---
+++ Ex003_QuantityElements/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write($"{element} ");
+}
+
+int QuantityEvenElements(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            quantity++;
+    }
+    return quantity;
+}
+
+int QuantityUnevenElements(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+            quantity++;
+    }
+    return quantity;
+}
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[n];
+FillArray(array);
+Console.Write("Массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел = {QuantityEvenElements(array)}");
+Console.WriteLine($"Количество нечётных чисел = {QuantityUnevenElements(array)}");
