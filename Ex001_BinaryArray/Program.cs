b33079e093783ff825676def8ed822592b1236af
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 15:09:27 +0300

    Бинарный массив из 8 элементов

---
+++ Ex001_BinaryArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+Console.Clear();
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0,2);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write($"{element} ");
+}
+
+Console.Clear();
+
+int[] array = new int[8];
+FillArray(array);
+Console.Write("Массив: ");
+PrintArray(array);
