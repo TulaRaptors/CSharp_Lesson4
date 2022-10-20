7ec8dc541aecab3ac1510296a7d2f57a1a5e3dd0
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 16:31:02 +0300

    Количество элементов из отрезка [10,99]

---
+++ Ex004_FindNumbersArray/Program.cs
@@ -0,0 +1,33 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write($"{element} ");
+}
+
+int FindElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+
+int[] array = new int[123];
+FillArray(array);
+Console.Write("Массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество элементов из отрезка [10,99] = {FindElements(array)}");
